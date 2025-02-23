43d6a5efb6aa57c70507b392d111c6cea056c03b
puttipongchut <puttipongyrc@gmail.com>
2025-02-24 01:01:32 +0700

    Fixed routing navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import './App.css';
 import Login from "./pages/LoginPage";
