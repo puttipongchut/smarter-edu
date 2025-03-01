634a53d97bbaa0012da40c3000b1e2d28adc2c56
puttipongchut <puttipongyrc@gmail.com>
2025-03-02 02:42:49 +0700

    changed cors origin to vercel app

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://smarter-edu-backend.vercel.app/'
+  ? 'https://smarter-edu-backend.onrender.com'
   : 'http://localhost:5000';
 
 export default defineConfig({
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(cors({
-    origin: 'https://smarter-edu-backend.vercel.app',
+    origin: 'https://smarter-edu.vercel.app',
     credentials: true,
   }));
 
